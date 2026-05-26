2edfd9b38fa49ffa5004a4dc0d61fe5abc3d8093
FyodorGV <fedorgreben17@mail.ru>
2026-05-26 10:55:22 +0300

    Добавлен класс Controller

--- src/main/java/gui/GameWindow.java
+++ src/main/java/gui/GameWindow.java
@@ -2,20 +2,31 @@ package gui;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Map;
 
+/**
+ * Окно игры с роботом. Содержит визуализатор и связывается с контроллером.
+ */
 public class GameWindow extends JInternalFrame implements Saveble
 {
     private final GameVisualizer gameVisualizer;
+    private final Controller controller;
+    private final RobotModel model;
 
     public GameWindow() 
     {
         super("Игровое поле", true, true, true, true);
-        gameVisualizer = new GameVisualizer();
+        model = new RobotModel();
+        gameVisualizer = new GameVisualizer(model);
+        controller = new Controller(model, gameVisualizer);
+
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(gameVisualizer, BorderLayout.CENTER);
         getContentPane().add(panel);
         pack();
+    }
+
+    public RobotModel getModel() {
+        return model;
     }
 
     @Override
